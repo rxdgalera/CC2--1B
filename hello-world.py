ee31909cb3834b1d741f82f0a843fb925c106a5b
Rupert Xavier D. Galera <145944433+rxdgalera@users.noreply.github.com>
2023-09-27 08:48:55 +0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+#concatinate- plus sign, curly string
+
+#enter name
+name = input("What is your name?")
+#enter age
+age_input = input("What is your age?")
+
+#convert age
+age = int(age_input)
+age_next_year = age + 1
+
+pi = 3.1416
+
+print(f"Hello, {name}")
+
+
+#mathematical operators- +, -, *, /, %
